2c1926fb9f9873a4fab128c0a47917f164afdf20
Anitha Edith <anitha.edith@team.telstra.com>
2018-09-27 17:37:11 +0530

    redisson

--- src/main/kotlin/ips/Application.kt
+++ src/main/kotlin/ips/Application.kt
@@ -1,11 +1,41 @@
 package ips
 
+import org.redisson.api.RedissonClient
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.SpringApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
+// import org.springframework.data.redis.connection.RedisConnectionFactory
+// import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+import org.redisson.config.Config
+import org.redisson.Redisson
+
+
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
+}
+
+/*
+@Bean
+@Profile("dev")
+fun jedisConnectionFactory(): RedisConnectionFactory {
+    val factory = JedisConnectionFactory()
+    factory.hostName = "127.0.0.1"
+    factory.port = 6379
+    factory.usePool = true
+    return factory
+}*/
+
+@Bean
+@Profile("dev")
+fun redissonClient(): RedissonClient  {
+    val config = Config()
+    val hostUri = "rediss://127.0.0.1:6379"
+    config.useReplicatedServers().addNodeAddress(hostUri)
+    val client = Redisson.create(config)
+    return client
 }
--- src/main/kotlin/ips/caching/controller.kt
+++ src/main/kotlin/ips/caching/controller.kt
@@ -4,22 +4,36 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import javax.annotation.Resource
-import org.springframework.data.redis.core.SetOperations
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.core.HashOperations
-import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.data.redis.core.SetOperations
+//import org.springframework.data.redis.core.RedisTemplate
+//import org.springframework.data.redis.core.HashOperations
+//import org.springframework.data.redis.core.StringRedisTemplate;
+import org.bouncycastle.crypto.tls.ConnectionEnd.client
+import org.redisson.api.RLocalCachedMap
+import org.redisson.api.RedissonClient
+import org.redisson.api.LocalCachedMapOptions
+import org.bouncycastle.crypto.tls.ConnectionEnd.client
+import org.jboss.logging.MDC.getMap
+import org.redisson.api.RMap
 
 @RestController
 class HelloController {
 
-    @Autowired
+  /*  @Autowired
     private val template: RedisTemplate<String, String>? = null
 
     @Autowired
-    private val stringRedisTemplate: StringRedisTemplate? = null
+    private val stringRedisTemplate: StringRedisTemplate? = null*/
 
-    private var hashOperations: HashOperations<String, String, Long>? = null
+    @Autowired
+    private val redissonClient: RedissonClient? = null
+
+   // private var hashOperations: HashOperations<String, String, Long>? = null
+
+    var options = LocalCachedMapOptions.defaults().maxIdle((10 * 1000).toLong()).timeToLive((10 * 1000).toLong())
+    var map: RMap<String?, String?>? = redissonClient?.getMap("myredisCache")
+
 
     @GetMapping("/hello")
     fun helloKotlin(): String {
@@ -32,6 +46,16 @@ class HelloController {
         return "hello world" + name
     }
 
+    @GetMapping("/get/{cacheitem}")
+    fun setCache(@PathVariable("cacheitem") cacheitem: String?): String? {
+        println("before pushing to cache")
+        map?.get(cacheitem)
+        println("pushed to cache" + map?.get(cacheitem))
+        return cacheitem
+    }
+
+
+/*
     @GetMapping("/set/{cacheitem}")
     fun setCache(@PathVariable("cacheitem") cacheitem: String?): String? {
         println("before pushing to cache")
@@ -64,6 +88,6 @@ class HelloController {
         stringRedisTemplate?.opsForValue()?.set(cacheitem, cacheitem )
         println("setstring pushed to cache")
         return "hello world"
-    }
+    }*/
 
 }
